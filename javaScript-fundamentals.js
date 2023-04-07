38536e3bf7196d237eff9f9d3aad0568a5ade142
ariful171042 <arif171042@gmail.com>
2023-04-07 12:23:01 +0600

    javaScript fundamentals

---
+++ javaScript-fundamentals.js
@@ -0,0 +1,35 @@
+/* //variabels
+
+//var
+var number = 100;
+console.log(number);
+//let
+
+let friendName = "maruf";
+console.log(friendName);
+
+// console.log(window);
+
+//const 
+//const is not 
+const temp = 25.5;
+console.log(temp);
+
+test = "error";
+console.log(test); */
+
+//hoisted
+// var variabels hoisted to window
+//var is up to upper step
+
+//data types
+//primitive ,non primitive
+
+//primitive
+//number
+//string
+//boolean
+
+const name = "maruf";
+
+console.log(name);
